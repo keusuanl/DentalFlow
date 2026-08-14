ef8b927677ed65d3955336502af484682f29c8b0
keusuanl <k.usuanlele@outlook.com>
2026-08-14 12:45:38 +0100

    infra: add S3/SQS/SNS/RDS outputs for backend integration

--- infra/envs/dev/outputs.tf
+++ infra/envs/dev/outputs.tf
@@ -2,3 +2,40 @@ output "alb_dns_name" {
   description = "Public DNS name of the Application Load Balancer"
   value       = module.ecs.alb_dns_name
 }
+
+output "s3_bucket_id" {
+  description = "Name of the S3 bucket for scan storage"
+  value       = module.s3.bucket_id
+}
+
+output "s3_bucket_arn" {
+  description = "ARN of the S3 bucket for scan storage"
+  value       = module.s3.bucket_arn
+}
+
+output "upload_queue_url" {
+  description = "URL of the SQS upload event queue"
+  value       = module.sqs_sns.upload_queue_url
+}
+
+output "upload_queue_arn" {
+  description = "ARN of the SQS upload event queue"
+  value       = module.sqs_sns.upload_queue_arn
+}
+
+output "notification_topic_arn" {
+  description = "ARN of the SNS topic for lab/clinic notifications"
+  value       = module.sqs_sns.notification_topic_arn
+}
+
+output "db_endpoint" {
+  description = "RDS PostgreSQL endpoint"
+  value       = module.rds.db_endpoint
+}
+
+output "db_secret_arn" {
+  description = "ARN of the Secrets Manager secret holding DB credentials"
+  value       = module.rds.db_secret_arn
+  sensitive   = true
+}
+
